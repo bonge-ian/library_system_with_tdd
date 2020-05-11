8ebd701fad843d3a814719cf0764d60157c859bc
Bonge Ian <ikaranjah@zoho.com>
2020-05-11 13:31:16 +0300

    w/p for the book resource

--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -7,23 +7,22 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class BookReservationTest extends TestCase
+class BookManagementTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
     public function a_book_can_be_added_to_the_library()
     {
-        $this->withoutExceptionHandling();
+        // $this->withoutExceptionHandling();
         $response = $this->post('/book', [
             'title' => 'Book one',
             'author' => 'Sony vaio'
         ]);
-
-        // assert for successful reponse
-        $response->assertOk();
-
+        $book = Book::first();
+      
         $this->assertCount(1, Book::all());
+        $response->assertRedirect($book->path());
     }
 
    /** @test */
@@ -59,21 +58,37 @@ class BookReservationTest extends TestCase
     /** @test */
     public function a_book_can_be_updated()
     {
-        $this->withoutExceptionHandling();
         $this->post('/book', [
             'title' => 'Cool Book',
             'author' => 'Rey'
         ]);
         $book = Book::first();
 
-        $response = $this->patch('/book/' . $book->id, [
+        $response = $this->patch($book->path(), [
             'title' => 'New Book',     
             'author' => 'Ndungu'
         ]);
 
-        $response->assertOk();
-        $this->assertEquals('New Book', Book::first()->title);
-        $this->assertEquals('Ndungu', Book::first()->author);
+        $this->assertEquals('New Book', $book->fresh()->title);
+        $this->assertEquals('Ndungu', $book->fresh()->author);
+        $response->assertRedirect($book->fresh()->path());
+    }
 
+    /** @test */
+    public function a_book_can_be_deleted()
+    {
+        $this->post('/book', [
+            'title' => 'Cool Book',
+            'author' => 'Rey'
+        ]);
+        $book = Book::first();
+
+        // assert if post request was a success
+        $this->assertCount(1, Book::all());
+
+        $response = $this->delete($book->path());
+
+        $this->assertCount(0, Book::all());
+        $response->assertRedirect('/books');
     }
 }
