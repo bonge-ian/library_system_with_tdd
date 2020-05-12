44df3a6141583c13d70283a8eab4bdea766ff5f0
Bonge Ian <ikaranjah@zoho.com>
2020-05-12 16:15:47 +0300

    Make auth and checkout feature test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/book', 'BooksController@store');
@@ -8,3 +9,10 @@ Route::delete('/book/{book}', 'BooksController@destroy');
 
 // author routes
 Route::post('/author', 'AuthorsController@store');
+
+Route::post('/checkout/{book}', 'CheckoutBookController@store');
+Route::post('/checkin/{book}', 'CheckinBookController@store');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
