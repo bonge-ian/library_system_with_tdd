0cfd865b5417a6cdb57b62bd96c09c53b3638eae
Bonge Ian <ikaranjah@zoho.com>
2020-05-12 16:17:39 +0300

    Revert "Make auth and checkout feature test"
    This reverts commit 44df3a6141583c13d70283a8eab4bdea766ff5f0.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/book', 'BooksController@store');
@@ -9,10 +8,3 @@ Route::delete('/book/{book}', 'BooksController@destroy');
 
 // author routes
 Route::post('/author', 'AuthorsController@store');
-
-Route::post('/checkout/{book}', 'CheckoutBookController@store');
-Route::post('/checkin/{book}', 'CheckinBookController@store');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
